3cafcb8b7620f9b125485d84a070fd173e9587cb
Nick Tillinghast <nick.tillinghast@thekrazycouponlady.com>
2021-12-02 14:10:37 -0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
+
 
 const app = express();
 const client = redis.createClient({
@@ -11,7 +13,8 @@ client.set('visits', 0);
 
 
 app.get('/', (req, res) => {
-   client.get('visits', (err, visits) => {
+    process.exit(0);
+    client.get('visits', (err, visits) => {
     res.send('Number of visits is ' + visits); 
     client.set('visits', parseInt(visits) + 1);
     });
